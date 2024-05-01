cb411f554d988db972e384bb920060daeac56636
Khant Thit Oo <trav.whoami@gmail.com>
2024-05-01 10:26:41 +0630

    Router implemented

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Signup from './pages/SignUp/Signup';
+
+const App = () => (
+  <Routes>
+    <Route path='/' element={<h1>We'll make it later Go to /login or /signup</h1>} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+  </Routes>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
